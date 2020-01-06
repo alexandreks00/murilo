be0fcc403ef9b9c40d2245eb1f57858f28e17923
Gustavo <guuqueiroz@hotmail.com>
2020-01-06 18:57:44 -0300

    Criação tc's ValidaCriticaCriacaoUsuarioCpfRepetido e ValidaCriticaCriacaoUsuarioEmailRepetido + finalização tc ValidaAlterarSenhaUsuario

--- EcommerceBackend/Users.cs
+++ EcommerceBackend/Users.cs
@@ -38,7 +38,7 @@ namespace DemoRestSharp
         public void ValidaInformacoesLoginUsuario()
         {
             ExtentTest test = null;
-            test = extent.CreateTest("ValidaInformacoesLoginUsuario").Info("Início do teste");
+            test = extent.CreateTest("ValidaInformacoesLoginUsuario").Info("Início do teste.");
 
             try
             {
@@ -92,7 +92,7 @@ namespace DemoRestSharp
         public void ValidaEsqueciMinhaSenha()
         {
             ExtentTest test = null;
-            test = extent.CreateTest("ValidaEsqueciMinhaSenha").Info("Início do teste");
+            test = extent.CreateTest("ValidaEsqueciMinhaSenha").Info("Início do teste.");
 
             try 
             {
@@ -166,7 +166,7 @@ namespace DemoRestSharp
         public void ValidaInformacoesUserId()
         {
             ExtentTest test = null;
-            test = extent.CreateTest("ValidaInformacoesUserId").Info("Início do teste");
+            test = extent.CreateTest("ValidaInformacoesUserId").Info("Início do teste.");
 
             try
             {
@@ -197,12 +197,12 @@ namespace DemoRestSharp
                 Assert.That(response.Data.Member.DateOfBirth, Is.EqualTo("1996-04-12T00:00:00Z"), "Valor da propriedade 'DateOfBirth' divergente.");
                 Assert.That(response.Data.Member.CityId, Is.EqualTo("12789"), "Valor da propriedade 'CityId' divergente.");
                 Assert.That(response.Data.Member.City.CityId, Is.EqualTo(12789), "Valor da propriedade 'City.CityId' divergente.");
-                Assert.That(response.Data.City.Name, Is.EqualTo("Taguatinga"), "Valor da propriedade 'City.Name' divergente.");
+                Assert.That(response.Data.Member.City.Name, Is.EqualTo("Taguatinga"), "Valor da propriedade 'City.Name' divergente.");
                 Assert.That(response.Data.Member.City.StateId, Is.EqualTo(7), "Valor da propriedade 'City.StateId' divergente.");
                 Assert.That(response.Data.Member.City.State.Code, Is.EqualTo("DF"), "Valor da propriedade 'City.State.Code' divergente.");
                 Assert.That(response.Data.Member.City.State.Name, Is.EqualTo("Distrito Federal"), "Valor da propriedade 'City.State.Name' divergente.");
                 Assert.That(response.Data.Member.City.State.CountryId, Is.EqualTo(0), "Valor da propriedade 'City.State.CountryId' divergente.");
-                Assert.That(response.Data.Member.Phone1, Is.EqualTo("11923236362525"), "Valor da propriedade 'Phone1' divergente.");
+                Assert.That(response.Data.Member.Phone1, Is.EqualTo("1136362525"), "Valor da propriedade 'Phone1' divergente.");
                 test.Log(Status.Pass, "Teste ok, todas as verificações foram realizadas com sucesso.");
             }
             catch(Exception e)
@@ -214,10 +214,10 @@ namespace DemoRestSharp
         }
 
         [Test]
-        public void ValidaCriacaoUsuario () 
+        public void ValidaCriacaoUsuario() 
         {
             ExtentTest test = null;
-            test = extent.CreateTest("ValidaCriacaoUsuario").Info("Início do teste");
+            test = extent.CreateTest("ValidaCriacaoUsuario").Info("Início do teste.");
 
             try 
             {
@@ -331,10 +331,169 @@ namespace DemoRestSharp
         }
 
         [Test]
-        public void ValidaAlteraDadosUsuario ()
+        public void ValidaCriticaCriacaoUsuarioEmailRepetido()
         {
             ExtentTest test = null;
-            test = extent.CreateTest("ValidaAlteraDadosUsuario").Info("Início do teste");
+            test = extent.CreateTest("ValidaCriticaCriacaoUsuarioEmailRepetido").Info("Início do teste.");
+
+            try
+            {
+                //Gerando um cpf aleatório que será utilizado na criação do usuário
+                test.Log(Status.Info, "Gerando CPF aleatório que será utilizado no request de criação do usuário.");
+                string cpf = utils.Utils.gerarCpf();
+                string emailJaCadastrado = "automaticusers@mailinator.com";
+
+                //Criando a requisição responsável por criar um usuário
+                test.Log(Status.Info, "Criando a requisição responsável por criar usuário.");
+                var client = new RestClient(ConfigurationManager.AppSettings["dnsSensedia"]);
+                var requestCriaUsuario = new RestRequest("bus/v1/users", Method.POST);
+                requestCriaUsuario.RequestFormat = DataFormat.Json;
+
+                //Montando o body da requisição que será enviada
+                requestCriaUsuario.AddJsonBody(new
+                {
+                    AppInfo = new
+                    {
+                        deviceModel = "Moto G Play",
+                        devicePlatform = "Android",
+                        deviceUUID = "62a0391e-9b4c-4870-ba11-40896b488506",
+                        version = "4.0.20",
+                    },
+                    DateOfBirth = "2002-11-04T00:00:00.000Z",
+                    CardNumber = "",
+                    City = new
+                    {
+                        CityId = 12789,
+                        Name = "Taguatinga",
+                        State = new
+                        {
+                            Code = "DF",
+                            StateId = 7,
+                            Name = "Distrito Federal"
+                        },
+                        StateId = 7
+                    },
+                    CityId = 12789,
+                    CPF = cpf,
+                    CpfNf = false,
+                    Email = emailJaCadastrado,
+                    EndUserPolicyId = 3,
+                    Gender = "M",
+                    Name = "Automatic Test",
+                    NickName = "QA",
+                    Password = "112233",
+                    Phone1 = "1133333336"
+                }
+                );
+
+                //Setando header de autenticação "X-CISIdentity"
+                test.Log(Status.Info, "Setando headers necessários para realizar a requisição.");
+                utils.Utils.setCisToken(requestCriaUsuario);
+
+                //Enviando a requisição
+                test.Log(Status.Info, "Enviando a requisição.");
+                var responseCriaUsuario = client.Execute<ModelUsers>(requestCriaUsuario);
+
+                //Validando Status Code de retorno da requisição
+                test.Log(Status.Info, "Validando se o Status Code de retorno da requisição é 400.");
+                Assert.That((int)responseCriaUsuario.StatusCode, Is.EqualTo(400), "Status Code diferente do esperado ao enviar requisição criando um usuário utilizando um e-mail já cadastrado.");
+
+                test.Log(Status.Info, "Validando se a mensagem de crítica é exibida.");
+                Assert.That(responseCriaUsuario.Data.Message, Is.EqualTo("O e-mail \""+emailJaCadastrado+"\" informado já está sendo utilizado! Por favor, informe um endereço de e-mail diferente."), "Valor da propriedade 'Message' divergente.");
+            }
+            catch (Exception e)
+            {
+                test.Log(Status.Fail, e.ToString());
+                throw new Exception("Falha ao validar a mensagem de crítica que é exibida ao criar um usuário utilizando e-mail já cadastrado: " + e.Message);
+            }
+        }
+
+        [Test]
+        public void ValidaCriticaCriacaoUsuarioCpfRepetido()
+        {
+            ExtentTest test = null;
+            test = extent.CreateTest("ValidaCriticaCriacaoUsuarioCpfRepetido").Info("Início do teste.");
+
+            try
+            {
+                //Gerando um cpf aleatório que será utilizado na criação do usuário
+                test.Log(Status.Info, "Gerando email aleatório que será utilizado no request de criação do usuário.");
+                string cpfJaCadastrado = "19100000000";
+                string email = utils.Utils.gerarEmailAleatorio(8);
+
+                //Criando a requisição responsável por criar um usuário
+                test.Log(Status.Info, "Criando a requisição responsável por criar um usuário utilizando cpf já existente.");
+                var client = new RestClient(ConfigurationManager.AppSettings["dnsSensedia"]);
+                var requestCriaUsuario = new RestRequest("bus/v1/users", Method.POST);
+                requestCriaUsuario.RequestFormat = DataFormat.Json;
+
+                //Montando o body da requisição que será enviada
+                requestCriaUsuario.AddJsonBody(new
+                {
+                    AppInfo = new
+                    {
+                        deviceModel = "Moto G Play",
+                        devicePlatform = "Android",
+                        deviceUUID = "62a0391e-9b4c-4870-ba11-40896b488506",
+                        version = "4.0.20",
+                    },
+                    DateOfBirth = "2002-11-04T00:00:00.000Z",
+                    CardNumber = "",
+                    City = new
+                    {
+                        CityId = 12789,
+                        Name = "Taguatinga",
+                        State = new
+                        {
+                            Code = "DF",
+                            StateId = 7,
+                            Name = "Distrito Federal"
+                        },
+                        StateId = 7
+                    },
+                    CityId = 12789,
+                    CPF = cpfJaCadastrado,
+                    CpfNf = false,
+                    Email = email,
+                    EndUserPolicyId = 3,
+                    Gender = "M",
+                    Name = "Automatic Test",
+                    NickName = "QA",
+                    Password = "112233",
+                    Phone1 = "1133333336"
+                }
+                );
+
+                //Setando header de autenticação "X-CISIdentity"
+                test.Log(Status.Info, "Setando headers necessários para realizar a requisição.");
+                utils.Utils.setCisToken(requestCriaUsuario);
+
+                //Enviando a requisição
+                test.Log(Status.Info, "Enviando a requisição.");
+                var responseCriaUsuario = client.Execute<ModelUsers>(requestCriaUsuario);
+
+                //Validando Status Code de retorno da requisição
+                test.Log(Status.Info, "Validando se o Status Code de retorno da requisição é 400.");
+                Assert.That((int)responseCriaUsuario.StatusCode, Is.EqualTo(400), "Status Code diferente do esperado ao enviar requisição criando um usuário utilizando um cpf já cadastrado.");
+
+                test.Log(Status.Info, "Validando se a mensagem de crítica é exibida.");
+                Assert.That(responseCriaUsuario.Data.Message, Is.EqualTo("O CPF informado (\"19100000000\") já está sendo utilizado pelo e-mail testeqa@mailinator.com. Caso não tenha mais acesso ao e-mail, entre em contato com o Fale Conosco."), "Valor da propriedade 'Message' divergente.");
+            }
+            catch (Exception e)
+            {
+                test.Log(Status.Fail, e.ToString());
+                throw new Exception("Falha ao validar a mensagem de crítica que é exibida ao criar um usuário utilizando e-mail já cadastrado: " + e.Message);
+            }
+
+
+        }
+        
+        
+        [Test]
+        public void ValidaAlteraDadosUsuario()
+        {
+            ExtentTest test = null;
+            test = extent.CreateTest("ValidaAlteraDadosUsuario").Info("Início do teste.");
             string emailAlterado = "automacaousers@mailinator.com";
             string emailRollback = "alterardadosrollback@mailinator.com";
 
@@ -425,22 +584,24 @@ namespace DemoRestSharp
             ExtentTest test = null;
             test = extent.CreateTest("ValidaAlteraSenhaUsuario").Info("Início do teste.");
             string email = "alterasenha@mailinator.com";
+            double userId = 6947512;
 
             try
             {
                 //Coletando token "authorization"
                 string authorizationToken = utils.Utils.getAuthorization(email,"112233");
 
-
-                //Criando e enviando a requisição
+                //Criando e enviando a requisição responsável por alterar a senha do usuário
                 test.Log(Status.Info, "Criando requisição responsável por alterar a senha do usuário.");
                 var client = new RestClient(ConfigurationManager.AppSettings["dnsSensedia"]);
                 var requestAlteraSenha = new RestRequest("bus/v1/users/changepassword", Method.POST);
                 requestAlteraSenha.RequestFormat = DataFormat.Json;
                 requestAlteraSenha.AddJsonBody(new
                 {
-                    Email = email,
-                    Password = "111111"
+                    UserId = userId,
+                    CurrentPassword = "112233", 
+                    NewPassword = "111111",
+                    PasswordConfirmation = "111111"
                 }
                 );
                 
@@ -453,7 +614,7 @@ namespace DemoRestSharp
 
                 //Início das validações
                 test.Log(Status.Info, "Validando se o Status Code de retorno da requisição é 200.");
-                Assert.That((int)responseAlteraSenha.StatusCode, Is.EqualTo(200), "Status Code divergente.");
+                Assert.That((int)responseAlteraSenha.StatusCode, Is.EqualTo(200), "Status Code diferente do esperado ao enviar a requisição responsável por realizar a alteração de senha.");
 
                 //Criando e enviando a requisição responsável por tentar realizar login com a nova senha
                 test.Log(Status.Info, "Criando requisição responsável por tentar realizar login com a senha nova.");
@@ -462,22 +623,47 @@ namespace DemoRestSharp
                 requestRealizaLoginSenhaAlterada.AddJsonBody(new
                 {
                     Email = email,
-                    Password = "112233"
+                    Password = "111111"
                 }
                 );
 
+                utils.Utils.setCisToken(requestRealizaLoginSenhaAlterada);
+
                 test.Log(Status.Info, "Enviando requisição ");
-                var responseRealizaLoginSenhaInvalida = client.Execute<ModelUsers>(requestRealizaLoginSenhaAlterada);
+                var responseRealizaLoginSenhaAlterada = client.Execute<ModelUsers>(requestRealizaLoginSenhaAlterada);
 
                 test.Log(Status.Info, "Validando se o Status Code de retorno da requisição é 200.");
-                Assert.That((int)responseRealizaLoginSenhaInvalida.StatusCode, Is.EqualTo(200), "Status Code divergente.");
+                Assert.That((int)responseRealizaLoginSenhaAlterada.StatusCode, Is.EqualTo(200), "Status Code diferente do esperado ao enviar a requisição responsável por realizar login com a senha que foi alterada.");
 
+                //Criando e enviando a requisição responsável por realizar rollback na senha do usuário
+                test.Log(Status.Info, "Criando requisição responsável por realizar rollback na senha do usuário.");
+                var requestRollbackSenha = new RestRequest("bus/v1/users/changepassword", Method.POST);
+                requestRollbackSenha.RequestFormat = DataFormat.Json;
+                requestRollbackSenha.AddJsonBody(new
+                {
+                    UserId = userId,
+                    CurrentPassword = "111111",
+                    NewPassword = "112233",
+                    PasswordConfirmation = "112233"
+                }
+                );
+
+                test.Log(Status.Info, "Setando headers necessários para realizar a requisição.");
+                utils.Utils.setCisToken(requestRollbackSenha);
+                utils.Utils.setAuthorizationToken(requestRollbackSenha, authorizationToken);
+
+                test.Log(Status.Info, "Enviando requisição.");
+                var responseRollbackSenha = client.Execute<ModelUsers>(requestRollbackSenha);
+
+                //Início das validações
+                test.Log(Status.Info, "Validando se o Status Code de retorno da requisição é 200.");
+                Assert.That((int)responseRollbackSenha.StatusCode, Is.EqualTo(200), "Status Code diferente do esperado ao enviar a requisição responsável por realizar o rollback da senha.");
 
             }
             catch (Exception e)
             {
                 test.Log(Status.Fail, e.ToString());
-                throw new Exception("Falha ao validar a alteração de senha do usuário " + e.Message);
+                throw new Exception("Falha ao validar a alteração de senha do usuário: " + e.Message);
             }
 
 
