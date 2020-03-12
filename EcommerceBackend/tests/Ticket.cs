07db5993da6d7ae3593d48851b6f271fdebaa69a
Roberto Guimarães Junior <rjunior@international.cinemark.com>
2020-03-12 11:18:04 -0300

    Criado variável de sessão

--- EcommerceBackend/tests/Ticket.cs
+++ EcommerceBackend/tests/Ticket.cs
@@ -40,6 +40,7 @@ namespace EcommerceBackend
         {
 
             ExtentTest test = null;
+            var IDSession = "031052EC-06B8-43DE-B95B-82A1950B2044";
             test = extent.CreateTest("ValidaTipoIngresso").Info("Início do teste.");
 
             try
@@ -47,7 +48,7 @@ namespace EcommerceBackend
                 //Criando e enviando requisição
                 test.Log(Status.Info, "Criando requisição responsável por realizar login.");
                 var client = new RestClient(ConfigurationManager.AppSettings["dnsSensedia"]);
-                var request = new RestRequest("ticket/v1/types?TheaterId=688&SessionCode=031052EC-06B8-43DE-B95B-82A1950B2044", Method.GET);
+                var request = new RestRequest("ticket/v1/types?TheaterId=688&SessionCode=" + IDSession, Method.GET);
                 request.RequestFormat = DataFormat.Json;
                 test.Log(Status.Info, "Setando headers necessários para realizar a requisição.");
                 Utils.setCisToken(request);
