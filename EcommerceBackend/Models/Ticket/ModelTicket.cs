38312ec10bc9fdad854bed5f90d1ead33f67550c
Alexandre <alexandre.ksss@gmail.com>
2021-02-10 17:14:57 -0300

    Alterando a validação de contrato em /order/list

--- EcommerceBackend/Models/Ticket/ModelTicket.cs
+++ EcommerceBackend/Models/Ticket/ModelTicket.cs
@@ -6,9 +6,35 @@ using System.Threading.Tasks;
 
 namespace DemoRestSharp.models.ModelTicket
 {
-    class ModelTicket
-    {
-
+    public class ModelTicket
+    {   
+        public Transacao transacao { get; set; }
+        public TransacaoSecundaria transacaoSecundaria { get; set; }
 
     }
+
+    public class Beneficio
+    {
+        public int Quantidade { get; set; }
+    }
+
+    public class Transacao
+    {
+        public int ParceiroId { get; set; }
+        public int Codigo { get; set; }
+        public string Mensagem { get; set; }
+        public List<Beneficio> Beneficios { get; set; }
+    }
+
+    public class TransacaoSecundaria
+    {
+        public int ParceiroId { get; set; }
+        public int Codigo { get; set; }
+        public string Mensagem { get; set; }
+        public List<Beneficio> Beneficios { get; set; }
+    }
+
+
+
+
 }
